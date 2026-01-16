772fcdc8c55adcac6c9c7c6f811db8047bc960f7
JaFr6000 <jafr6000@stud.hs-flensburg.de>
2026-01-16 22:27:38 +0100

    generic templates hinzugefügt und book list bearbeitet + book_details eingefügt - nächster step details für autor, publisher und reviews

--- bookdb/library/views.py
+++ bookdb/library/views.py
@@ -20,23 +20,22 @@ class BookListView(ListView):
 class BookDetailView(DetailView):
     model = BookItem
     template_name = "library/book/book_detail.html"
-    context_object_name = "book"
 
 class BookCreateView(CreateView):
     model = BookItem
     form_class = BookItemForm
-    template_name = "library/book/book_form.html"
+    template_name = "library/generic_forms.html"
     success_url = reverse_lazy("book_list")
 
 class BookUpdateView(UpdateView):
     model = BookItem
     form_class = BookItemForm
-    template_name = "library/book/book_form.html"
+    template_name = "library/generic_forms.html"
     success_url = reverse_lazy("book_list")
 
 class BookDeleteView(DeleteView):
     model = BookItem
-    template_name = "library/book/book_confirm_delete.html"
+    template_name = "library/generic_confirm_delete.html"
     success_url = reverse_lazy("book_list")
 # -------------
 # Author CRUD
@@ -48,24 +47,24 @@ class AuthorListView(ListView):
 
 class AuthorDetailView(DetailView):
     model = Author
-    template_name = "library/author/author_detail.html"
+    template_name = "library/generic_detail.html"
     context_object_name = "author"
 
 class AuthorCreateView(CreateView):
     model = Author
     form_class = AuthorForm
-    template_name = "library/author/author_form.html"
+    template_name = "library/generic_forms.html"
     success_url = reverse_lazy("author_list")
 
 class AuthorUpdateView(UpdateView):
     model = Author
     form_class = AuthorForm
-    template_name = "library/author/author_form.html"
+    template_name = "library/generic_forms.html"
     success_url = reverse_lazy("author_list")
 
 class AuthorDeleteView(DeleteView):
     model = Author
-    template_name = "library/author/author_confirm_delete.html"
+    template_name = "library/generic_confirm_delete.html"
     success_url = reverse_lazy("author_list")
 # -------------
 # Publisher CRUD
@@ -77,24 +76,24 @@ class PublisherListView(ListView):
 
 class PublisherDetailView(DetailView):
     model = Publisher
-    template_name = "library/publisher/publisher_detail.html"
+    template_name = "library/generic_detail.html"
     context_object_name = "publisher"
 
 class PublisherCreateView(CreateView):
     model = Publisher
     form_class = PublisherForm
-    template_name = "library/publisher/publisher_form.html"
+    template_name = "library/generic_forms.html"
     success_url = reverse_lazy("publisher_list")
 
 class PublisherUpdateView(UpdateView):
     model = Publisher
     form_class = PublisherForm
-    template_name = "library/publisher/publisher_form.html"
+    template_name = "library/generic_forms.html"
     success_url = reverse_lazy("publisher_list")
 
 class PublisherDeleteView(DeleteView):
     model = Publisher
-    template_name = "library/publisher/publisher_confirm_delete.html"
+    template_name = "library/generic_confirm_delete.html"
     success_url = reverse_lazy("publisher_list")
 # -------------
 # Review CRUD
@@ -106,22 +105,22 @@ class ReviewListView(ListView):
 
 class ReviewDetailView(DetailView):
     model = Review
-    template_name = "library/reviews/review_detail.html"
+    template_name = "library/generic_detail.html"
     context_object_name = "review"
 
 class ReviewCreateView(CreateView):
     model = Review
     form_class = ReviewForm
-    template_name = "library/reviews/review_form.html"
+    template_name = "library/generic_forms.html"
     success_url = reverse_lazy("review_list")
 
 class ReviewUpdateView(UpdateView):
     model = Review
     form_class = ReviewForm
-    template_name = "library/reviews/review_form.html"
+    template_name = "library/generic_forms.html"
     success_url = reverse_lazy("review_list")
 
 class ReviewDeleteView(DeleteView):
     model = Review
-    template_name = "library/reviews/review_confirm_delete.html"
+    template_name = "library/generic_confirm_delete.html"
     success_url = reverse_lazy("review_list")
